4f103f87acd77af0b5d2c4c7b2dff46327387f57
chen.dong <chen_dong85@foxmail.com>
2017-07-30 16:24:02 +0800

    use express as the server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-(() => console.log('Hello SSR'))();
+import express from 'express';
+
+const app =  express();
+
+app.set('host', process.env.HOST || '0.0.0.0');
+app.set('port', process.env.PORT || '8080');
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World !')
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`The App is Lisitening at ${app.get('port')}`)
+})
